5ca61b39386d215918a935aca60486d415aa9176
Alexander Acosta <maestropuffins@gmail.com>
2022-12-02 16:21:42 -0500

    Added ost file

--- seq_pert_paths.m
+++ seq_pert_paths.m
@@ -1,3 +1,4 @@
 addpath (genpath ('C:\speechres\audapter_matlab'))
 addpath (genpath ('C:\speechres\audapter_mex'))
 addpath (genpath ('C:\speechres\commonmcode'))
+addpath (genpath ('C:\AudDev\code\experiment'))
