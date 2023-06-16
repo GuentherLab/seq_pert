be2680e8f532ffd14e04eec3a7731829bd536ed1
Xernam6 <73004390+Xernam6@users.noreply.github.com>
2023-06-16 13:31:36 -0400

    .

--- code/experiment/seq_pert_stim_list_gen/stimListGenWIP.m
+++ code/experiment/seq_pert_stim_list_gen/stimListGenWIP.m
@@ -1,14 +1,34 @@
-function stimList = stimListGenWIP(numTrials)
+function stimStruct = generateStimStruct(numTrials)
     % Calculate the number of trials for each condition
     numTrialsN1 = round(numTrials * 0.5);
     numTrialsU1 = round(numTrials * 0.25);
     numTrialsD1 = round(numTrials * 0.25);
     
     % Create an initial sequence with repeated conditions
-    sequence = repmat(["N1"; "U1"; "D1"], ceil(numTrials/3), 1);
+    sequence = ["N1"; "N1"; "U1"; "D1"];
+    
+    % Extend the sequence to match the desired number of trials
+    numRepetitions = ceil(numTrials / numel(sequence));
+    sequence = repmat(sequence, numRepetitions, 1);
     
     % Shuffle the sequence
     sequence = sequence(randperm(numel(sequence)));
+    
+    % Ensure the desired distribution of values
+    excessTrials = numel(sequence) - numTrials;
+    if excessTrials > 0
+        % Remove excess trials from N1 condition
+        n1Indices = find(sequence == "N1");
+        removeIndices = n1Indices(1:excessTrials);
+        sequence(removeIndices) = [];
+    elseif excessTrials < 0
+        % Add missing trials to N1 condition
+        missingTrials = -excessTrials;
+        n1Indices = find(sequence == "N1");
+        addIndices = n1Indices(1:missingTrials);
+        sequence = [sequence; repmat("N1", missingTrials, 1)];
+        sequence(addIndices) = sequence(randperm(numel(addIndices)));
+    end
     
     % Ensure no condition repeats more than 3 times
     for i = 4:numel(sequence)
@@ -24,5 +44,5 @@ function stimList = stimListGenWIP(numTrials)
     sequence = sequence(1:numTrials);
     
     % Create the struct with the randomized sequence
-    stimList = struct('Condition', sequence);
+    stimStruct = struct('Condition', sequence);
 end
