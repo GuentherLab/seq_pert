a905077307159c7309b6efa899a311f24578f14f
Xernam6 <73004390+Xernam6@users.noreply.github.com>
2023-08-04 13:48:24 -0400

    .

--- code/experiment/seq_pert_stim_list_gen/matchWords.m
+++ code/experiment/seq_pert_stim_list_gen/matchWords.m
@@ -1,24 +1,25 @@
-function matched_words = matchWords(excel_file, matlab_structure)
+function [output] = matchWords(excel_file, stimCondition)
 
-% Import data from excel file
-excel_data = readtable(excel_file);
+% Read data from the Excel file
+data = readtable(excel_file);
 
-% Get the orthography column from the excel data
-orthography = excel_data.orthography;
+% Initialize the output table
+output = [];
 
-% Get the stim type column from the matlab structure
-stim_type = matlab_structure.stim_type;
+% Loop through the stimCondition structure
+for i = 1:size(stimCondition,1)
 
-% Create a new MATLAB structure to store the matched words
-matched_words = struct();
+    % Get the current condition
+    condition = stimCondition(i).condition;
 
-% Iterate through the orthography column and match the words to the stim type
-for i = 1:length(orthography)
-  word = orthography(i);
-  stim_type_index = find(strcmp(word, stim_type));
-  if stim_type_index ~= -1
-     matched_words.(stim_type(stim_type_index)) = word;
-  end
+    % Get a random word from the corresponding condition in the Excel file
+    row = data(data.stim_types == condition, :);
+    index = randsample(size(row,1),1);
+    word = row(index,:);
+
+    % Add the word to the output table
+    output = [output; word];
+
 end
 
 end
