1cd7015eb2c348d6be4dc1c930787373f057f8ed
Adanna Thomas <84044347+ajacosta929@users.noreply.github.com>
2024-06-26 12:48:21 -0400

    .

--- code/analysis/onesub_flvoice_firstlevel.m
+++ code/analysis/onesub_flvoice_firstlevel.m
@@ -2,6 +2,6 @@ subject = 'sub-sp001';
 session = 'ses-2';
 run = 'run-2';
 
-flvoice_firstlevel(subject,session,run,'aud-reflexive','F1_up_down','F1-mic',{'D1','U1'},[1,-1])
-flvoice_firstlevel(subject,session,run,'aud-reflexive','F1_up_compenstion','F1-mic',{'U1','N1'},[1,-1])
-flvoice_firstlevel(subject,session,run,'aud-reflexive','F1_down_compenstion','F1-mic',{'D1','N1'},[1,-1])
+% flvoice_firstlevel(subject,session,run,'aud-reflexive','F1_up_down','F1-mic',{'D1','U1'},[1,-1])
+% flvoice_firstlevel(subject,session,run,'aud-reflexive','F1_up_null','F1-mic',{'U1','N1'},[1,-1])
+flvoice_firstlevel(subject,session,run,'aud-reflexive','F1_down_null','F1-mic',{'D1','N1'},[1,-1])
