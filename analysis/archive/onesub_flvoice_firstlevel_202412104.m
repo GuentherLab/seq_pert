5ed46addb9e28d64c7c99e6f42c6c66b19d72b00
amsmeier <amsmeier@users.noreply.github.com>
2025-03-20 13:33:27 -0400

    .

--- analysis/archive/onesub_flvoice_firstlevel_202412104.m
+++ analysis/archive/onesub_flvoice_firstlevel_202412104.m
@@ -1,13 +1,21 @@
-subject = 'sub-sp001';
-session = 'ses-2';
-run = 'run-2';
-
-% subject = 'sub-sp002';
+% subject = 'sub-sp001';
 % session = 'ses-2';
-% run = 'run-3';
+% run = 'run-2';
 
-% flvoice_firstlevel(subject,session,run,'aud-reflexive','F1_up_down','F1-mic',{'D1','U1'},[1,-1])
-% flvoice_firstlevel(subject,session,run,'aud-reflexive','F1_up_null','F1-mic',{'U1','N1'},[1,-1])
-flvoice_firstlevel(subject,session,run,'aud-reflexive','F1_down_null','F1-mic',{'D1','N1'},[1,-1])
+subject = 'sub-sp002';
+session = 'ses-2';
+run = 'run-3';
+
+% op.sub = 'sp003';
+% op.ses = 2;
+% op.run = 2;
+
+% op.sub = 'sp004';
+% op.ses = 2;
+% op.run = 2;
+
+% flvoice_firstlevel(op.sub,op.ses,run,'aud-reflexive','F1_up_down','F1-mic',{'D1','U1'},[1,-1])
+% flvoice_firstlevel(op.sub,op.ses,run,'aud-reflexive','F1_up_null','F1-mic',{'U1','N1'},[1,-1])
+flvoice_firstlevel(op.sub,op.ses,run,'aud-reflexive','F1_down_null','F1-mic',{'D1','N1'},[1,-1])
 
 % flvoice_firstlevel(subject,session,run,'aud-reflexive','f1comp_down_up','f1comp',{'D1','U1'},[1,-1])
