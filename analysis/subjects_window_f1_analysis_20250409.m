c6762789fba7451bb13b662e99087393e727d824
amsmeier <amsmeier@users.noreply.github.com>
2025-04-09 18:04:49 -0400

    .

---
+++ analysis/subjects_window_f1_analysis_20250409.m
@@ -0,0 +1,7 @@
+dirs = setDirs_seq_pert();
+
+
+subject_table_master_file = [dirs.projRepo, filesep, 'subject_analysis_master.csv']; 
+
+subs = readtable(subject_table_master_file, "FileType","text", "Delimiter",'comma')
+subs = subs(logical(subs.analyze),:);
